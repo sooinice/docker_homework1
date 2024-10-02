633b9a6f715a1405ca643e4193f0be87b5c09343
sooinice <sooin9674@naver.com>
2024-10-02 14:55:03 +0900

    Modified addition feature-branch2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
 
-    addition_result = num1 + num2
+   addition_result = (num1 + num2) + 10
     subtraction_result = num1 - num2
 
     with open('./result/file.txt', 'w') as f:
